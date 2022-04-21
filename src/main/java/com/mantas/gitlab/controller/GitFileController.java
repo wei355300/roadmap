e4f424f76db8b2a39fdf76da677b39b2563aca53
feng.wei <feng.wei@gmail.com>
2022-04-21 21:11:59 +0800

    git file name encode(base64)

--- src/main/java/com/mantas/gitlab/controller/GitFileController.java
+++ src/main/java/com/mantas/gitlab/controller/GitFileController.java
@@ -2,12 +2,16 @@ package com.mantas.gitlab.controller;
 
 import com.mantas.controller.R;
 import com.mantas.gitlab.service.GitFileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/git/file")
 public class GitFileController {
@@ -18,13 +22,17 @@ public class GitFileController {
         this.gitFileService = domainService;
     }
 
+    /**
+     *
+     * @param file 存放在Git上的文件, 为避免文件后缀等影响, 需要将文件名参数在http传输时先base64编码
+     * @return
+     * @throws Exception
+     */
     @GetMapping("/{file}")
     public R<String> getGitFile(@PathVariable("file") String file) throws Exception {
-        return R.success(gitFileService.getContent(file));
+        byte[] c = Base64.getDecoder().decode(file);
+        String fileName = new String(c);
+        log.info("获取Git文件: {}", fileName);
+        return R.success(gitFileService.getContent(fileName));
     }
-
-//    @GetMapping("/{key}/detail")
-//    public R<String> getDomainDetail(@PathVariable("key") String domainKey) throws Exception {
-//        return R.success(gitFileService.getContent("domains.json", "master"));
-//    }
 }
