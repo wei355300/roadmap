8731cb0abc1313030b8013aca4d58041746d5947
feng.wei <feng.wei@gmail.com>
2022-04-23 14:20:57 +0800

    update domains

--- src/main/java/com/mantas/gitlab/controller/GitFileController.java
+++ src/main/java/com/mantas/gitlab/controller/GitFileController.java
@@ -5,11 +5,11 @@ import com.mantas.gitlab.service.GitFileService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Base64;
+import java.net.URLDecoder;
 
 @Slf4j
 @RestController
@@ -23,15 +23,13 @@ public class GitFileController {
     }
 
     /**
-     *
      * @param file 存放在Git上的文件, 为避免文件后缀等影响, 需要将文件名参数在http传输时先base64编码
      * @return
      * @throws Exception
      */
-    @GetMapping("/{file}")
-    public R<String> getGitFile(@PathVariable("file") String file) throws Exception {
-        byte[] c = Base64.getDecoder().decode(file);
-        String fileName = new String(c);
+    @GetMapping("/")
+    public R<String> getGitFile(@RequestParam("file") String file) throws Exception {
+        String fileName = URLDecoder.decode(file, "UTF-8");
         log.info("获取Git文件: {}", fileName);
         return R.success(gitFileService.getContent(fileName));
     }
