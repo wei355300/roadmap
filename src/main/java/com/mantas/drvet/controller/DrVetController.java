72642fe172a7d2e9006d56737ec3c1d79877daa6
feng.wei <feng.wei@gmail.com>
2022-11-27 23:07:18 +0800

    维特医生接口

--- src/main/java/com/mantas/drvet/controller/DrVetController.java
+++ src/main/java/com/mantas/drvet/controller/DrVetController.java
@@ -4,6 +4,8 @@ import com.mantas.controller.R;
 import com.mantas.drvet.mapper.DrvetMobileMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/base/api/drvet")
@@ -15,12 +17,22 @@ public class DrVetController {
         this.mapper = mapper;
     }
 
-    @GetMapping("match/mobile")
+    @GetMapping("/match/mobile")
     public R matchMobile(@RequestParam("mobile") String mobile) {
         int count = mapper.countByMobile(mobile);
         if (count > 0) {
             return R.success();
         }
         return R.result(-1, "");
+    }
+
+    @PostMapping("/add/mobile")
+    public R addMobile(@RequestParam("mobile") String mobile) {
+        String m = mapper.getMobile(mobile);
+        if (Objects.nonNull(m)) {
+            return R.result(-1, "手机号已存在");
+        }
+        mapper.addMobile(mobile);
+        return R.success();
     }
 }
--- src/main/java/com/mantas/drvet/mapper/DrvetMobileMapper.java
+++ src/main/java/com/mantas/drvet/mapper/DrvetMobileMapper.java
@@ -1,12 +1,16 @@
 package com.mantas.drvet.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface DrvetMobileMapper {
 
     @Select("select count(1) from drvet_mobile where mobile=#{mobile}")
     int countByMobile(@Param("mobile") String mobile);
+
+    @Select("select * from drvet_mobile where mobile=#{mobile}")
+    String getMobile(@Param("mobile") String mobile);
+
+    @Insert("insert into drvet_mobile (mobile) values (#{mobile})")
+    void addMobile(@Param("mobile") String mobile);
 }
